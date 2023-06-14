19aac18ba871773a90f1491fec833ac7049bbfd2
chiba <sc87holl@gmail.com>
2023-06-14 18:54:02 +0800

    update:v2.0 Alpha auto language

--- src/components/ts/useStoage.ts
+++ src/components/ts/useStoage.ts
@@ -1,6 +1,5 @@
 import {useStorage} from "@vueuse/core";
 
-console.log(navigator.language)
 let browserLang: string = navigator.language.substring(0, 2)
 if (navigator.language.substring(0, 2) != "zh" && "en") {
     browserLang = "en"
