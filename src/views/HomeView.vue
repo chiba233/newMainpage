30947b36b38a22ef99c99f95ecbacc9bde81112e
chiba <qwq@qwwq.org>
2026-02-12 04:20:10 +0700

    v7.0.3 fix ThemeIndex type bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -74,7 +74,7 @@ const blog: Record<string, string> = {
   other: "blog",
 } as const;
 
-type ThemeIndex = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+type ThemeIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 type BackgroundKey = `background${ThemeIndex}`;
 type ColorData = Record<BackgroundKey, string>;
 const randomTheme = Math.floor(Math.random() * 9);
